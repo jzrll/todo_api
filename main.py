c4815bc9f2bd10eca249607cab3af02151e063f0
jzrll <jetjetllumbera@gmail.com>
2026-07-16 21:52:11 +0800

    Stage 4: full CRUD

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ tasks = [
     {"id": 3, "title": "Task 3", "done": False},
 ]
 
-
 @app.get("/")
 async def read_root():
     return { "name": "Task API", "version": "1.0", "endpoints": ["/tasks"] }
@@ -40,7 +39,7 @@ async def create_task(task: dict):
             content={"error": "Task title is required"}
         )
     new_task = {
-        "id": len(tasks) + 1,
+        "id": max([task["id"] for task in tasks], default=0) + 1,
         "title": task.get("title"),
         "done": False
     }
@@ -48,5 +47,34 @@ async def create_task(task: dict):
     return JSONResponse(
         status_code=201,
         content= {"message": "Created"}
+    )
+
+@app.put("/tasks/{task_id}")
+async def update_task(task_id: int, task: dict):
+    for t in tasks:
+        if t["id"] == task_id:
+            t["title"] = task.get("title", t["title"])
+            t["done"] = task.get("done", t["done"])
+            return JSONResponse(
+                status_code=200,
+                content={"message": "Updated"}
+            )
+    return JSONResponse(
+        status_code=404,
+        content={"error": f"Task {task_id} not found"}
+    )
+
+@app.delete("/tasks/{task_id}")
+async def delete_task(task_id: int):
+    for t in tasks:
+        if t["id"] == task_id:
+            tasks.remove(t)
+            return JSONResponse(
+                status_code=200,
+                content={"message": "Deleted"}
+            )
+    return JSONResponse(
+        status_code=404,
+        content={"error": f"Task {task_id} not found"}
     )
 
